7d25ac384df654574609642ef4915ad5d81d46e8
svsp <77344156+german142000@users.noreply.github.com>
2024-04-12 04:51:08 +0500

    Delete scrt-system_v3_host directory

--- scrt-system_v3_host/getKeyId.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("config.php");
-ini_set('display_errors', 1);
-$database = $scrt_config['db'];
-$result = $database->sendSQLRequest("SELECT * FROM SCRTprivateCertificate");
-$result = mysqli_fetch_assoc($result);
-$data = json_encode(array("scrt_version" => $scrt_config["version"], "keyId" => $result['keyId']));
-echo $data;
-?>
